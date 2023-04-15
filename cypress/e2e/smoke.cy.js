2784fe62e86708be9ecc41c48fc24862843b9ca8
mohammedyh <mohammedyousufhamid@gmail.com>
2023-04-16 00:55:34 +0100

    Add e2e tests and start unit tests

---
+++ cypress/e2e/smoke.cy.js
@@ -0,0 +1,39 @@
+/// <reference types="Cypress" />
+
+describe('app', () => {
+	it('loads the form', () => {
+		cy.visit('/');
+		cy.get('#card-name').should('exist');
+		cy.get('#card-number').should('exist');
+		cy.get('input[name="expiryMonth"]').should('exist');
+		cy.get('input[name="expiryYear"]').should('exist');
+		cy.get('#cvc-number').should('exist');
+		cy.get('button').contains(/confirm/i);
+	});
+
+	it('shows all error messages when form details are invalid', () => {
+		cy.visit('/');
+		cy.get('button')
+			.contains(/confirm/i)
+			.click();
+		cy.contains(/can't be blank/i).should('exist');
+		cy.contains(/credit card number is invalid/i).should('exist');
+		cy.contains(/invalid month/i).should('exist');
+		cy.contains(/invalid year/i).should('exist');
+		cy.contains(/invalid cvc/i).should('exist');
+	});
+
+	it('shows thank you message when form details are valid', () => {
+		cy.visit('/');
+		cy.get('#card-name').type('Elon Musk');
+		cy.get('#card-number').type('4111 1111 1111 1111');
+		cy.get('input[name="expiryMonth"]').type('04');
+		cy.get('input[name="expiryYear"]').type(new Date().getFullYear() + 2);
+		cy.get('#cvc-number').type('123');
+		cy.get('button')
+			.contains(/confirm/i)
+			.click();
+		cy.contains(/thank you/i).should('exist');
+		cy.contains(/We've added your card details/i).should('exist');
+	});
+});
---
+++ src/components/CardForm.test.js
@@ -0,0 +1,7 @@
+/// <reference types="vitest" />
+
+import { expect, test } from 'vitest';
+
+test('temp', () => {
+	return expect(2).toBe(2);
+});
